64a2487185df35363d9d6db07d95946bbbe2abc7
jpsdr <jpsdr.psx@free.fr>
2022-02-05 09:58:29 +0100

    Add a feature.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

--- ThreadPoolDLL/ThreadPoolDLL.h
+++ ThreadPoolDLL/ThreadPoolDLL.h
@@ -40,6 +40,7 @@ typedef struct _UserData
 {
 	uint16_t UserId;
 	bool AllowSeveral;
+	bool AllowWaiting;
 	int8_t NbrePool;
 	int8_t UsedPool[MAX_THREAD_POOL];
 } UserData;
@@ -97,6 +98,8 @@ class ThreadPoolInterface
 	bool EnableAllowSeveral(uint16_t UserId);
 	bool DisableAllowSeveral(uint16_t UserId);
 	bool IsAllowedSeveral(uint16_t UserId);
+	bool EnableWaitonRequest(uint16_t UserId);
+	bool DisableWaitonRequest(uint16_t UserId);
 	int8_t GetPoolAllocated(uint16_t UserId);
 	int8_t GetPoolNumber(uint16_t UserId,int8_t idxPool);
 	int8_t GetPoolIndex(uint16_t UserId,int8_t nPool);
